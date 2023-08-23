8dea78e918677e1eac0a10b1a4f7f7ca989b6568
Rishavbagga77 <41142557+Rishavbagga77@users.noreply.github.com>
2023-08-23 10:38:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,3 @@
-#!/usr/bin/env python3
-"""Usage:
-  main.py pull (<type>) [--tag tag]... [--dry-run] [-h]
-  main.py push (<type>) [--dry-run] [-h]
-
-Examples:
-    Dashboards:
-        main.py pull dashboards
-        main.py push dashboards
-
-    Synthetic api tests using --tag that only pulls tests if all the defined tags exist on them:
-        main.py pull synthetic_api_tests p --tag env:production --tag application:abc
-        main.py push synthetic_api_tests
-
-    Run with --dry-run without making any changes to your Datadog account:
-        main.py pull dashboards --dry-run
-        main.py push dashboards --dry-run
- 
-    Supported arguments:
-    main.py pull|push dashboards|monitors|users|synthetics_api_tests|synthetics_browser_tests|awsaccounts|logpipelines|notebooks (--tag tag) (--dry-run|-h)
-
-    Note. --tag is currently only supported for synthetics_api_tests, synthetics_browser_tests and monitors.
-
-Options:
-  -h, --help
-  -d, --dry-run
-  --tag=<k:v>   Specify a tag in the format key:value
-"""
-
 from docopt import docopt
 import json
 import os
